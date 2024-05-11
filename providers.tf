9f37bb769343d9e1dbee7354225f86f5857a35bb
prabhu <prabhu.arjunan@cdw.com>
2024-05-11 10:51:42 +0530

    new

--- providers.tf
+++ providers.tf
@@ -9,13 +9,11 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  profile = "Helen"
   # Configuration options
 }
 
 provider "aws" {
   alias = "west"
   region = "us-west-1"
-  profile = "Helen"
 }
 
